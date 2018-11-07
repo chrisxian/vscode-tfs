6a4f646abff2368c3f9b2711a568c06b40947123
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-07 18:14:18 +0100

    lib/undo: refac

--- src/lib/undo.ts
+++ src/lib/undo.ts
@@ -5,20 +5,19 @@ import * as vscode from 'vscode'
  * Undo current file pending changes.
  */
 export default function(itemspec: string[]): void {
-  vscode.window.setStatusBarMessage('TFS: Undoing...')
+  vscode.window.setStatusBarMessage(`TFS: Undoing...`)
 
-  var callback = function(responseError, response) {
-    if (responseError) {
+  // tslint:disable-next-line:no-any
+  tfs('undo', itemspec, null, (err: any) => {
+    if (err) {
       vscode.window.setStatusBarMessage(null)
-      vscode.window.showErrorMessage('TFS: ' + responseError.error)
+      vscode.window.showErrorMessage(`TFS: ${err.error}`)
 
       return
     }
 
-    vscode.window.setStatusBarMessage('TFS: Undo successful.')
+    vscode.window.setStatusBarMessage(`TFS: Undo successful.`)
 
-    vscode.window.showInformationMessage('TFS: Pending changes undone.')
-  }
-
-  tfs('undo', itemspec, null, callback)
+    vscode.window.showInformationMessage(`TFS: Pending changes undone.`)
+  })
 }
