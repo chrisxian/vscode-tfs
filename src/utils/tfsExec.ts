f768ae988ed3e9929bb61c51f95eee6931c92856
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-07 17:03:48 +0100

    utils/tfsExec: switch to TS full

--- src/utils/tfsExec.ts
+++ src/utils/tfsExec.ts
@@ -1,15 +1,17 @@
-var utils  = require('./common'),
-    vscode = require('vscode');
+import * as vscode from 'vscode'
+
+import utils from './common'
 
 /**
  * Execute a TFS command.
  */
-export default function(command: string, isGlobal: boolean = false) {
+export default function(command: string, isGlobal: boolean = false): void {
   if (!isGlobal && !utils.getCurrentFilePath()) {
-    vscode.window.showErrorMessage('TFS: Either there is no current file opened or your current file has not been saved.');
-    return;
+    vscode.window.showErrorMessage('TFS: Either there is no current file opened or your current file has not been saved.')
+
+    return
   }
 
-  var itemspec = isGlobal ? utils.getCurrentWorkspacePath() : utils.getCurrentFilePath();
-  require('../tfs/' + command)([itemspec]);
+  var itemspec = isGlobal ? utils.getCurrentWorkspacePath() : utils.getCurrentFilePath()
+  require('../tfs/' + command)([itemspec])
 }
