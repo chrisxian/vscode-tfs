d2a956ea7e2242fae302beb1a08f4366b8bc1e20
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-07 17:38:06 +0100

    lib: create index

---
+++ src/lib/index.ts
@@ -0,0 +1,15 @@
+import add from './add'
+import checkin from './checkin'
+import checkout from './checkout'
+import get from './get'
+import status from './status'
+import undo from './undo'
+
+export {
+  add,
+  checkin,
+  checkout,
+  get,
+  status,
+  undo,
+}
