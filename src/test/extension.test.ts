0e2fcc8cb5c08b6a642856640277a0478cb3a080
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-07 20:38:59 +0100

    test/extension.test: refac

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,7 +1,5 @@
 import * as assert from 'assert'
 
-suite("TFS Extension Tests", () => {
-  test("A dummy test", async () => {
-    assert.equal(2, 1 + 1);
-  })
+suite('TFS Extension Tests', () => {
+  test('A dummy test', async () => assert.equal(2, 1 + 1))
 })
