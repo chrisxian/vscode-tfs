73839e9f8646aec0dc86d857dae5675a17619f1b
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-07 18:12:54 +0100

    lib/checkout: refac

--- src/lib/checkout.ts
+++ src/lib/checkout.ts
@@ -5,13 +5,11 @@ import * as vscode from 'vscode'
  * Checkout current file for edit.
  */
 export default function(itemspec: string[]): void {
-  var callback = function(responseError): void {
-    if (responseError) {
-      return
-    }
+  // tslint:disable-next-line:no-any
+  tfs('checkout', itemspec, null, (err: any) => {
+    if (err) return
 
-    vscode.window.setStatusBarMessage('TFS: ' + itemspec[0].substr(itemspec[0].lastIndexOf('/') + 1) + ' has been automatically checked out for editing.')
-  }
-
-  tfs('checkout', itemspec, null, callback)
+    const fileName = itemspec[0].substr(itemspec[0].lastIndexOf('/') + 1)
+    vscode.window.setStatusBarMessage(`TFS: ${fileName} has been automatically checked out for editing.`)
+  })
 }
