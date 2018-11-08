76c9cf2bc33f609ad070e6daa6961a6d862e244e
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-08 02:12:58 +0100

    utils/statusManager: make workspace path resolution local

--- src/utils/statusManager.ts
+++ src/utils/statusManager.ts
@@ -26,14 +26,18 @@ const statusManager = {
   },
 
   list: (): string[] => {
-    if (!utils.fileExists(vscodeTfsFilePath)) return []
+    if (vscode.workspace.workspaceFolders === undefined) return []
 
+    const vscodeTfsFilePath = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.vscodetfs`
     const content = JSON.parse(fs.readFileSync(vscodeTfsFilePath, 'utf8'))
 
     return content.excludedFiles !== undefined ? content.excludedFiles : []
   },
 
   save: (excludedFiles: string[]): void => {
+    if (vscode.workspace.workspaceFolders === undefined) return
+
+    const vscodeTfsFilePath = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.vscodetfs`
     fs.writeFileSync(vscodeTfsFilePath, JSON.stringify({ excludedFiles }, null, 2), 'utf8')
   },
 }
